5206fd88c99d9542a30bf37e69f469404d00f9be
Juan Carlos Avendaño Valenzuela <109890592+juank329@users.noreply.github.com>
2023-01-22 21:57:16 -0300

    ejercicio 01 conjuntos

---
+++ conjuntos.js
@@ -0,0 +1,22 @@
+// Crear un nuevo Set con los nombres de tu familia
+let familia = new Set(["Juan", "Maria", "Pedro", "Sofia"]);
+
+// Modificar el Set original añadiendo tu nombre (duplicado)
+familia.add("Juan");
+
+// Modificar el Set original añadiendo el nombre "Javascript"
+familia.add("Javascript");
+
+console.log(familia);
+Nota: En este código, el nombre "Juan" ya existe en el conjunto original, por lo que al agregarlo de nuevo no causará ningún cambio en la cantidad de elementos del conjunto. El nombre "Javascript" sera agregado exitosamente.
+
+
+
+
+Crea un archivo llamado conjuntos.js que contenga las siguientes líneas
+
+- Un nuevo Set con los nombres de tu familia
+
+- Modifica el Set original añadiendo tu nombre (duplicado) (debería darte lo mismo)
+
+- Modifica el Set original añadiendo el nombre "Javascript" (ya que empieza a formar parte de tu vida ;)
